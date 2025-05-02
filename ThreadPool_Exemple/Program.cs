32a1828e559807d4be44911d9089a5de860b3a91
Eduard Ruesga <eduard.ruesga@itb.cat>
2025-05-02 08:41:14 +0200

    ThreadPool Exemple Added

---
+++ ThreadPool_Exemple/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ThreadPool_Exemple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int WorkCount = 100;
+
+            for (int i = 0; i < WorkCount; i++)
+            {
+                //Cridem al Pool i posem en cua el treball a fer.
+                ThreadPool.QueueUserWorkItem(ExecuteWorkInPool, i); //Requereix que el metode cridat tingui un objecte per paramentre
+            }
+
+        }
+
+        static void ExecuteWorkInPool(object o)
+        {
+            int i = (int)o;
+            Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} starts the work number: {i}");
+            Thread.Sleep(1000);
+        }
+        static void ClassicThreading(int workCount)
+        {
+            for (int i = 0; i < workCount; i++)
+            {
+                Thread t = new Thread(ExecuteWork);
+                t.Start();
+            }
+        }
+
+        static void ExecuteWork()
+        {
+            Console.WriteLine($"Thread: {Thread.CurrentThread.ManagedThreadId} starts work");
+            Thread.Sleep(1000);
+        }
+    }
+}
